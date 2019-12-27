9fe70946c40ba8f12e83edd8ab9b7c9a5fc7a4d0
ukabyl <umkabylbekov@gmail.com>
2019-12-27 10:14:14 +0600

    Site was completed

---
+++ page/js/common.js
@@ -0,0 +1,42 @@
+const vm = new Vue({
+	el: "#page",
+	data: {
+		product: 1
+	},
+	mounted() {
+		this.$nextTick(function() {
+			// *****************************************Anchor
+      $(".anchor").on("click", function (event) {
+          event.preventDefault();
+          var id  = $(this).attr('href'),
+              top = $(id).offset().top;
+          $('body,html').animate({scrollTop: top}, 1000);
+      });
+      // *****************************************Anchor
+
+      $('.callback-round__trigger').click(function() {
+		      $( this ).parent().toggleClass('active');
+		  })
+
+		  $('form').submit(function (e) {
+	    let form_data = $(this).serialize();
+	    $.ajax({
+	        type: "POST",
+	        url: "feedback.php",
+	        data: form_data,
+	        success: function () {
+	            $('form').trigger("reset");
+	            $('#modal').modal('hide');
+	            setTimeout(function() {
+	                $('#success').modal('show');
+	            }, 500);
+	            setTimeout(function() {
+	                $('#success').modal('hide');
+	            }, 4000);
+	        }
+	    });
+	    return false;
+		});
+		})
+	}
+});
